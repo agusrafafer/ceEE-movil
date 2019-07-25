86f3b4f5d09147caea2d41e4e8f02b5bef36eec3
jps <jps@jps-Lenovo-B50-70>
2019-07-25 12:57:18 -0300

    se actualiza ip por nic (escolarweb.com.ar)

--- public_html/js/factories.js
+++ public_html/js/factories.js
@@ -25,13 +25,13 @@ angular.module('app.factories', [])
         .factory('wsFactory', function () {
             return {
 //                url: "http://localhost:8080/escuelasparroquiales/webresources"
-                url: "http://108.160.155.140:8080/escuelasparroquiales/webresources"
+                url: "http://www.escolarweb.com.ar:8080/escuelasparroquiales/webresources"
             };
         })
         
         .factory('urlFotoFactory', function () {
             return {
-                url: "http://108.160.155.140:8080/escuelasparroquiales/faces/resources/images/fotos/"
+                url: "http://www.escolarweb.com.ar:8080/escuelasparroquiales/faces/resources/images/fotos/"
             };
         });
 
