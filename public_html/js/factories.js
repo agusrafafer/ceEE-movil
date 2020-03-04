7db8669aa9a1e387b8623c1df471875ed6c3b443
jps <jps@jps-Lenovo-B50-70>
2020-03-04 09:26:35 -0300

    Version 006 con google-service.json adaptado a AZULES

--- public_html/js/factories.js
+++ public_html/js/factories.js
@@ -25,7 +25,7 @@ angular.module('app.factories', [])
 
         .factory('wsFactory', function () {
             return {
-                //url: "http://localhost:8080/azules/webresources"
+                //url: "http://66.97.37.195:8080/azules/webresources"
                 url: "http://108.160.155.140:8080/azules/webresources"
             };
         })
@@ -33,6 +33,7 @@ angular.module('app.factories', [])
         .factory('urlFotoFactory', function () {
             return {
                 url: "http://108.160.155.140:8080/azules/faces/resources/images/fotos/"
+//         url: "http://66.97.37.195:8080/azules/faces/resources/images/fotos/"
             };
         });
 
